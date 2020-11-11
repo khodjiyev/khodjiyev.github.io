3928ff40dd08395590d6a74380e7f2f26777ee55
khodjiyev <oybekkhodjiyev@gmail.com>
2020-11-11 12:57:16 +0500

    changed now

---
+++ js/script.js
@@ -0,0 +1,10 @@
+const body = document.body;
+const btn = document.querySelectorAll('.button')[0];
+
+btn.addEventListener('mouseenter', () => {
+	body.classList.add('show');
+});
+
+btn.addEventListener('mouseleave', () => {
+	body.classList.remove('show');
+});
