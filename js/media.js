b2f5178d5e5b6ffbb52f00f106c9fccd47f9b976
khodjiyev <oybekkhodjiyev@gmail.com>
2020-11-17 17:01:40 +0500

    12121

---
+++ js/media.js
@@ -0,0 +1,17 @@
+    // var videoPlayer = document.getElementById('video-1');
+
+    // // Auto play, half volume.
+    // videoPlayer.play()
+    // videoPlayer.volume = 0.5;
+
+    // // Play / pause.
+    // videoPlayer.addEventListener('click', function () {
+    //     if (videoPlayer.paused == false) {
+    //         videoPlayer.pause();
+    //         videoPlayer.firstChild.nodeValue = 'Play';
+    //     } else {
+    //         videoPlayer.play();
+    //         videoPlayer.firstChild.nodeValue = 'Pause';
+    //     }
+    // });
+
